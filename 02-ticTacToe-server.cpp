a410179669cf0953e293a2cf7bd9a386df5f3a78
Lucas <lucasmagonzalez@alumno.unlam.edu.ar>
2025-05-28 20:58:12 -0300

    Tateti

--- 02-ticTacToe-server.cpp
+++ 02-ticTacToe-server.cpp
@@ -1,5 +1,5 @@
 // Servidor de Tres en Raya con sockets TCP y threads en C++
-
+#include <algorithm>
 #include <iostream>
 #include <string>
 #include <cstring>
@@ -45,6 +45,30 @@ void close_client(int client_fd) {
     connection_slots.release();
 }
 
+bool check_winner(char board[3][3], char sym) {
+    for (int i = 0; i < 3; ++i) {
+        if (board[i][0] == sym && board[i][1] == sym && board[i][2] == sym) return true; // fila
+        if (board[0][i] == sym && board[1][i] == sym && board[2][i] == sym) return true; // columna
+    }
+    if (board[0][0] == sym && board[1][1] == sym && board[2][2] == sym) return true; // diagonal principal
+    if (board[0][2] == sym && board[1][1] == sym && board[2][0] == sym) return true; // diagonal secundaria
+    return false;
+}
+
+string format_board(const char board[3][3]) {
+    string result;
+    for (int i = 0; i < 3; ++i) {
+        result += board[i][0];
+        result += ' ';
+        result += board[i][1];
+        result += ' ';
+        result += board[i][2];
+        result += '\n';
+    }
+    return result;
+}
+
+
 void handle_game(Game* game) {
     int current_client = game->turn == 0 ? game->client1 : game->client2;
     int opponent = game->turn == 0 ? game->client2 : game->client1;
@@ -62,6 +86,8 @@ void handle_game(Game* game) {
         }
 
         string cmd(buffer);
+        cmd.erase(std::remove(cmd.begin(), cmd.end(), '\r'), cmd.end());
+        cmd.erase(std::remove(cmd.begin(), cmd.end(), '\n'), cmd.end());
         if (cmd.starts_with("MOVE")) {
             int r, c;
             if (sscanf(buffer, "MOVE %d %d", &r, &c) == 2 && r >= 0 && r < 3 && c >= 0 && c < 3) {
@@ -69,11 +95,21 @@ void handle_game(Game* game) {
                 if (game->board[r][c] == ' ') {
                     game->board[r][c] = game->turn == 0 ? 'X' : 'O';
                     send(current_client, "+OK\n", 4, 0);
-                    string move_msg = "MOVE " + to_string(r) + " " + to_string(c) + " " + (game->turn == 0 ? "X\n" : "O\n");
+
+                    char sym = game->turn == 0 ? 'X' : 'O';
+                    if (check_winner(game->board, sym)) {
+                        send(current_client, "+WIN\n", 5, 0);
+                        send(opponent, "+LOSE\n", 6, 0);
+                        break;
+                    }
+                    
+                    string move_msg = "MOVE " + to_string(r) + " " + to_string(c) + " " + string(1, sym) + "\n";
                     send(opponent, move_msg.c_str(), move_msg.size(), 0);
+
                     game->turn = 1 - game->turn;
                     swap(current_client, opponent);
                     send(current_client, "+TURN\n", 6, 0);
+
                 } else {
                     send(current_client, "-ERR Invalid move\n", 19, 0);
                 }
@@ -84,7 +120,11 @@ void handle_game(Game* game) {
             send(current_client, "+BYE\n", 5, 0);
             send(opponent, "+RIVAL_QUIT\n", 13, 0);
             break;
-        } else {
+        }else if (cmd.starts_with("BOARD")) {
+            lock_guard<mutex> lock(game->game_mutex);
+            string board_str = format_board(game->board);
+            send(current_client, board_str.c_str(), board_str.size(), 0);
+        }else {
             send(current_client, "-ERR Unknown\n", 13, 0);
         }
     }
@@ -122,6 +162,8 @@ void handle_client(int client_fd) {
         if (len <= 0) break;
 
         string cmd(buffer);
+        cmd.erase(remove(cmd.begin(), cmd.end(), '\r'), cmd.end());
+       cmd.erase(remove(cmd.begin(), cmd.end(), '\n'), cmd.end());
         if (cmd.starts_with("PLAY")) {
             send(client_fd, "+WAIT\n", 6, 0);
             {
@@ -159,13 +201,16 @@ int main() {
     thread matchmaking_thread(matchmaker);
 
     while (server_running) {
-        sockaddr_in client_addr;
-        socklen_t addr_len = sizeof(client_addr);
-        connection_slots.acquire();
-        int client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &addr_len);
-        thread client_thread(handle_client, client_fd);
-        client_thread.detach();
+    connection_slots.acquire();
+
+    sockaddr_in client_addr;
+    socklen_t addr_len = sizeof(client_addr);
+
+    int client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &addr_len);
+    thread client_thread(handle_client, client_fd);
+    client_thread.detach();
     }
+
 
     matchmaking_thread.join();
     close(server_fd);
